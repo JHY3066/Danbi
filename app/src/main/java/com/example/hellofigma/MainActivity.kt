dabe6f2d66d56eb45a25919a4483266e742476c1
JHY3066 <jkjjjfamily30@gmail.com>
2023-11-10 23:22:12 +0900

    Loading & MainScreen

--- app/src/main/java/com/example/hellofigma/MainActivity.kt
+++ app/src/main/java/com/example/hellofigma/MainActivity.kt
@@ -22,12 +22,12 @@ import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
-import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.hellofigma.ui.theme.HelloFigmaTheme
 import com.example.hellofigma.mainscreen.Mainscreen
+
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -39,17 +39,13 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colors.background
                 ) {
-                    Mainscreen()
+                  Mainscreen()
                 }
             }
         }
     }
 }
 
-@Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
-}
 
 @Preview(showBackground = true)
 @Composable
